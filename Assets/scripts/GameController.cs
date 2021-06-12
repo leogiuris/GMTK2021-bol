a7ff9cea5d292f44c7feafd810fb4db7afad231f
brumach <brumach527@gmail.com>
2021-06-12 01:08:32 -0300

    kkk

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    //controle camera
+    private GameObject camera;
+    private Transform dialogPoint;
+    private Transform gamePoint;
+    //estado do jogo
+    private bool dialog = true;
 
+    //data
     public Hand playerHand, cpuHand;
     public Player p;
     bool handsHolding;
@@ -14,14 +21,27 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        camera = GameObject.Find("Main Camera");
         canHold = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Gamestate();
 
 
+    }
+    
+    private void Gamestate()
+    {
+        if (dialog)
+        {
+            camera.transform.position = dialogPoint.position;
+        }
+        else
+        {
+            camera.transform.position = gamePoint.position;
+        }
     }
 }
