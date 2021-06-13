cc6b8a05d3828c4363f60427b1d7cdac1a02cf49
leogiuris <leogiuris@gmail.com>
2021-06-12 23:30:24 -0300

    barra de vida

--- Assets/scripts/HUD.cs
+++ Assets/scripts/HUD.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour
 {
     public GameObject scoreBar;
+    public List<Sprite> smileys;
+    public GameObject knob;
 
     
+
 
     public void updateGotasCounter(int n)
     {
@@ -25,7 +28,18 @@ public class HUD : MonoBehaviour
 
     void Update()
     {
+
+        
+
         scoreBar.GetComponent<Slider>().value = GameController.Instance.awkwardness;
+
+        if (scoreBar.GetComponent<Slider>().value < 1)
+            knob.GetComponent<Image>().sprite = smileys[0];
+        else if (scoreBar.GetComponent<Slider>().value < 2)
+            knob.GetComponent<Image>().sprite = smileys[1];
+        else
+            knob.GetComponent<Image>().sprite = smileys[2];
+
         Debug.Log(GameController.Instance.awkwardness);
     }
 
