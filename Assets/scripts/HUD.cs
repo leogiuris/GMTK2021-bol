f25b62bc1d4eccf943f6ad92ada1a8eb3af54e84
leogiuris <leogiuris@gmail.com>
2021-06-12 23:02:51 -0300

    hud

---
+++ Assets/scripts/HUD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HUD : MonoBehaviour
+{
+    public GameObject scoreBar;
+
+    
+
+    public void updateGotasCounter(int n)
+    {
+        //gotas.text = ":  " + n + "/" + GameController.Instance.maxGotas;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        scoreBar.GetComponent<Slider>().value = GameController.Instance.awkwardness;
+        Debug.Log(GameController.Instance.awkwardness);
+    }
+
+}
